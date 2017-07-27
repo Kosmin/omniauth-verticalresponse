416e1a6276b1933d1025cac815ea6dee9be0548d
Paweł Gościcki <pawel.goscicki@gmail.com>
2017-07-27 18:12:26 +0200

    Fix compatibility with omniauth-oauth 1.4+
    References:
    * https://github.com/intridea/omniauth-oauth2/pull/70
    * https://github.com/intridea/omniauth-oauth2/commit/26152673224aca5c3e918bcc83075dbb0659717f
    * https://github.com/intridea/omniauth-oauth2/pull/82
    * https://github.com/jdennes/omniauth-createsend/issues/3
    * https://github.com/WebTheoryLLC/omniauth-twitch/pull/4
    * https://github.com/jdennes/omniauth-createsend/pull/5
    * https://github.com/DripEmail/omniauth-drip/pull/6

--- lib/omniauth/strategies/verticalresponse.rb
+++ lib/omniauth/strategies/verticalresponse.rb
@@ -10,11 +10,14 @@ module OmniAuth
           token_url:      'api/v1/oauth/access_token'
       }
       option :provider_ignores_state, true
-  
+
       uid do
         access_token.params['user_id']
       end
 
+      def callback_url
+        full_host + script_name + callback_path
+      end
     end
   end
 end
